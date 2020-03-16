b498e88391a01c447dcf508b7aa3971bf8c270b8
hpstream <915414300@qq.com>
2020-03-16 20:49:22 +0800

    feat: 初始化工程

---
+++ docs/quickstart.md
@@ -0,0 +1,11 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm 安装
+npm i vant-ui -S
+
+# 通过 yarn 安装
+yarn add vant-ui
+```
---
+++ vant.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: 'vant-ui',
+  build: {
+    css: {
+      preprocessor: 'less',
+    },
+    site: {
+      publicPath: '/vant-ui/',
+    },
+  },
+  site: {
+    title: 'vant-ui',
+    logo: 'https://img.yzcdn.cn/vant/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
